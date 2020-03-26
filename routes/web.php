e705957d790e532ae1cc9b84a7ad499de156827b
SteveNay <naylinaung.softwarearchitect@gmail.com>
2020-03-26 15:56:58 +0630

    [add] privacy policy

--- routes/web.php
+++ routes/web.php
@@ -22,22 +22,4 @@ Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
 
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-Route::get('test', function () {
-    // Create a client with a base URI
-    $client = new Client(['base_uri' => 'https://covid2019.leetdev.net/wp-json/wp/v2/']);
-    $response = $client->get('posts?categories=11&filter[orderby]=date&order=desc&per_page=1');
-    // Check if a header exists.
-    if ($response->hasHeader('content-type')) {
-        $body = $response->getBody();
-        $postList = json_decode($body, false);
-        if (count($postList)) {
-            $url = "https://covid2019.leetdev.net/archives/" . $postList[0]->id;
-        }
-    }
-    
-    if ($url) {
-        
-    }
-    $message =  "There is no updated news currently.";
-    echo ("No content");
-});
+Route::view('/privacy-policy', 'privacy_policy');
